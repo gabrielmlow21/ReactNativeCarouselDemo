9abb001349236ce621a2fcc75f57ebaea5148532
Gabriel Low <lowgabriel@ymail.com>
2023-01-03 20:53:33 +0800

    removed pagination and added vertical layout

--- CarouselCards.js
+++ CarouselCards.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { View } from "react-native";
-import Carousel from "react-native-snap-carousel";
-import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from "./CarouselCardItem";
+import Carousel, { Pagination } from "react-native-snap-carousel";
+import CarouselCardItem, {
+  ITEM_HEIGHT,
+  SLIDER_HEIGHT,
+} from "./CarouselCardItem";
 import data from "./data";
 
 const CarouselCards = () => {
+  const [index, setIndex] = React.useState(0);
   const isCarousel = React.useRef(null);
 
   return (
     <View>
       <Carousel
-        layout="tinder"
-        layoutCardOffset={9}
+        layout={"default"}
         ref={isCarousel}
         data={data}
         renderItem={CarouselCardItem}
-        sliderWidth={SLIDER_WIDTH}
-        itemWidth={ITEM_WIDTH}
-        inactiveSlideShift={0}
+        sliderHeight={SLIDER_HEIGHT}
+        itemHeight={ITEM_HEIGHT}
+        onSnapToItem={(index) => setIndex(index)}
         useScrollView={true}
+        vertical={true}
       />
     </View>
   );
