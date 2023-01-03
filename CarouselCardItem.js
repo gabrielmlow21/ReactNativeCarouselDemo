6872c987cc1f904d709aef0502755724ddc3265c
Gabriel Low <lowgabriel@ymail.com>
2023-01-03 20:54:07 +0800

    added height variables

--- CarouselCardItem.js
+++ CarouselCardItem.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet, Dimensions, Image } from "react-native";
 
-export const SLIDER_WIDTH = Dimensions.get("window").width + 80;
-export const ITEM_WIDTH = Math.round(SLIDER_WIDTH * 0.7);
+export const SLIDER_HEIGHT = Dimensions.get("window").height + 80;
+export const ITEM_HEIGHT = Math.round(SLIDER_HEIGHT * 0.7);
 
 const CarouselCardItem = ({ item, index }) => {
   return (
     <View style={styles.container} key={index}>
       <Image source={{ uri: item.imgUrl }} style={styles.image} />
       <Text style={styles.header}>{item.title}</Text>
-      <Text style={styles.body}>{item.body}</Text>
     </View>
   );
 };
@@ -27,6 +26,7 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.29,
     shadowRadius: 4.65,
+
     elevation: 7,
   },
   image: {
@@ -39,13 +39,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     paddingLeft: 20,
     paddingTop: 20,
-  },
-  body: {
-    color: "#222",
-    fontSize: 18,
-    paddingLeft: 20,
-    paddingLeft: 20,
-    paddingRight: 20,
   },
 });
 
